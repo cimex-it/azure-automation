d14355a8c5fcd908a55a8151f992112f21015c19
ondrej-vaclavu <ondrej.vaclavu@outlook.com>
2019-04-25 11:52:19 +0200

    first

---
+++ oms-automation/cleanup-metricdata/Remove-MetricDataTables.ps1
@@ -0,0 +1,16 @@
+# Script for removing metrics data from diagnostics storage accounts
+
+# Keep log data for the following amount of days
+$daysToKeep = 60
+
+$account = Get-AutomationConnection -Name AzureRunAsConnection
+Login-AzAccount -ServicePrincipal -Tenant $account.TenantID -ApplicationId $account.ApplicationID -CertificateThumbprint $account.CertificateThumbprint -Environment "AzureCloud"
+
+$removeDate = (Get-Date).AddDays(-$daysToKeep-10).ToString('yyyyMMdd')
+
+$storageAccounts = Get-AzStorageAccount
+
+foreach ($sa in $storageAccounts) {
+    $saContext = (Get-AzStorageAccount -ResourceGroupName $sa.ResourceGroupName -Name $sa.StorageAccountName).Context
+    Get-AzStorageTable -Context $saContext | where {$_.Name -match "^WADMetrics.*(\d\d\d\d\d\d\d\d)$" -and $Matches[1] -lt $removeDate | Remove-AzStorageTable}
+}
