a390e044f8bd352a2d418cdee396ac5eaa9b46d0
Ondrej Vaclavu <ondrej.vaclavu@outlook.com>
2024-02-05 13:48:48 +0100

    minor update

--- oms-automation/idm-createuser/Update-CloudUser.ps1
+++ oms-automation/idm-createuser/Update-CloudUser.ps1
@@ -24,7 +24,7 @@ try {
 Write-Output $user
 
 if ($PredUserEmail -ne $null) {
-  $groupMembership = (Get-MgUserMemberOfAsGroup -UserId $PredUserEmail | where {$_.OnpremisesSyncEnabled -ne $true -and $_.GroupTypes -notmatch "Dynamic" -and $_.Id -ne "1b539b43-c67f-4521-8316-f1da5de67cb8"}).Id
+  $groupMembership = (Get-MgUserMemberOfAsGroup -UserId $PredUserEmail | where {$_.OnpremisesSyncEnabled -ne $true -and $_.GroupTypes -notcontains "DynamicMembership" -and $_.Id -ne "1b539b43-c67f-4521-8316-f1da5de67cb8"}).Id
 
   Write-Output $groupMembership
 
@@ -32,3 +32,5 @@ if ($PredUserEmail -ne $null) {
     New-MgGroupMember -GroupId $groupId -DirectoryObjectId $user.Id
   }
 }
+
+
