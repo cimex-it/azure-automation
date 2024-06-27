9bd5cbc58ff6184e98d5a2536ed05b25e64fd735
Ondřej Václavů <ondrej.vaclavu@outlook.com>
2024-06-27 16:23:08 +0200

    Update Update-CloudUser.ps1

--- oms-automation/idm-createuser/Update-CloudUser.ps1
+++ oms-automation/idm-createuser/Update-CloudUser.ps1
@@ -23,6 +23,17 @@ try {
 
 Write-Output $user
 
+# Add Office 365 license
+$licenseGroup = Switch -Wildcard ($License) {
+    "Kiosk Online*"  {"License - O365 K1"}
+    "M365 Business Standard*" {"License - M365 Premium"}
+}
+
+If ($licenseGroup) {
+    $group = Get-MgGroup -Filter "Name eq '$licenseGroup'"
+    New-MgGroupMember -GroupId $group.Id -DirectoryObjectId $user.Id
+}
+
 if ($PredUserEmail -ne $null) {
   $groupMembership = Get-MgUserMemberOfAsGroup -UserId $PredUserEmail | where {$_.OnpremisesSyncEnabled -ne $true -and $_.GroupTypes -notcontains "DynamicMembership" -and $_.Id -ne "1b539b43-c67f-4521-8316-f1da5de67cb8"}
 
